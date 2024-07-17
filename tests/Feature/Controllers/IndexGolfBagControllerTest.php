05cbd723ed386b185ab80793c480636aac9744a7
bbowler1740 <bbowler1740@conestogac.on.ca>
2024-07-17 16:56:11 -0400

    add tests to IndexGolfBagControllerTest.php

--- tests/Feature/Controllers/IndexGolfBagControllerTest.php
+++ tests/Feature/Controllers/IndexGolfBagControllerTest.php
@@ -3,16 +3,53 @@
 namespace Tests\Feature\Controllers;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class IndexGolfBagControllerTest extends TestCase
 {
-    public function test_it_returns_all_golfbags_for_a_user(): void
+    use RefreshDatabase;
+    public function test_it_returns_all_golfbags_for_a_user_when_fewer_than_10_exist(): void
     {
-        $user = User::factory()->hasGolfBags(10)->create();
+        $numGolfbags = 5;
+        $user = User::factory()->hasGolfBags($numGolfbags)->create();
 
-        $this->actingAs($user)
+        $response = $this->actingAs($user)
             ->getJson(route('golfbags.index'))
             ->assertOk();
+
+        $response
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('meta')
+                     ->has('links')
+                     ->has('data', $numGolfbags));
+    }
+
+    public function test_it_does_not_allow_guest_access(): void
+    {
+        User::factory()->hasGolfBags(10)->create();
+
+        $this->getJson(route('golfbags.index'))
+            ->assertUnauthorized();
+    }
+
+    public function test_it_paginates_golfbag_index_responses_with_10_per_page_when_greater_than_10_exist(): void
+    {
+        $numGolfbags = 15;
+        $numGolfbagsPerPage = 10;
+        $user = User::factory()->hasGolfBags($numGolfbags)->create();
+
+        $this->assertDatabaseCount('golf_bags', $numGolfbags );
+
+        $response = $this->actingAs($user)
+            ->getJson(route('golfbags.index'))
+            ->assertOk();
+
+        $response
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('meta')
+                     ->has('links')
+                     ->has('data', $numGolfbagsPerPage));
     }
 }
