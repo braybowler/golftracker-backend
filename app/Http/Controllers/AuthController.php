18ca4f652be21b9023e9b770d8b4894bf3626cfb
bbowler1740 <bbowler1740@conestogac.on.ca>
2024-07-17 12:10:47 -0400

    update auth controller to remove non-existent api guard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
             'password' => $request->input('password'),
         ]);
 
-        auth()->guard('api')->login($user);
+        auth()->login($user);
 
         return response()->json([
             'user' => $user,
