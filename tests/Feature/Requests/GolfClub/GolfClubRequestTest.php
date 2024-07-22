8ef05b6e7d33d8b90fbd44db1860362ad989c833
bbowler1740 <bbowler1740@conestogac.on.ca>
2024-07-22 18:54:55 -0400

    finish tests for GolfClubController

--- tests/Feature/Requests/GolfClub/GolfClubRequestTest.php
+++ tests/Feature/Requests/GolfClub/GolfClubRequestTest.php
@@ -2,57 +2,178 @@
 
 namespace Tests\Feature\Requests\GolfClub;
 
+use App\Models\User;
 use Tests\TestCase;
 
 class GolfClubRequestTest extends TestCase
 {
     public function test_it_validates_the_make_attribute_of_a_golfclub_request_is_required()
     {
-        $this->markTestIncomplete('TODO');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => '',
+                    'model' => 'Test String',
+                    'loft' => 46,
+                    'carry_distance' => 130,
+                    'total_distance' => 130,
+                ]
+            )->assertUnprocessable();
     }
 
     public function test_it_validates_the_make_attribute_of_a_golfclub_request_must_be_a_string()
     {
-        $this->markTestIncomplete('TODO');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 10,
+                    'model' => 'Test String',
+                    'loft' => 46,
+                    'carry_distance' => 130,
+                    'total_distance' => 130,
+                ]
+            )->assertUnprocessable();
     }
 
     public function test_it_validates_the_model_attribute_of_a_golfclub_request_is_required()
     {
-        $this->markTestIncomplete('TODO');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => '',
+                    'loft' => 46,
+                    'carry_distance' => 130,
+                    'total_distance' => 130,
+                ]
+            )->assertUnprocessable();
     }
 
     public function test_it_validates_the_model_attribute_of_a_golfclub_request_must_be_a_string()
     {
-        $this->markTestIncomplete('TODO');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => 10,
+                    'loft' => 46,
+                    'carry_distance' => 130,
+                    'total_distance' => 130,
+                ]
+            )->assertUnprocessable();
     }
 
     public function test_the_carry_distance_attribute_of_a_golfclub_request_is_not_required()
     {
-        $this->markTestIncomplete('TODO');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => 'Test Model',
+                    'loft' => 46,
+                    'carry_distance' => null,
+                    'total_distance' => 130,
+                ]
+            )->assertCreated();
     }
 
     public function test_it_validates_that_the_carry_distance_attribute_of_a_golfbag_request_must_be_an_int_if_present()
     {
-        $this->markTestIncomplete('TODO');
-    }
+        $user = User::factory()->create();
 
-    public function test_it_validates_that_the_total_distance_attribute_of_a_golfbag_request_must_be_an_int_if_present()
-    {
-        $this->markTestIncomplete('TODO');
-    }
-
-    public function test_it_validates_that_the_loft_attribute_of_a_golfbag_request_must_be_an_int_if_present()
-    {
-        $this->markTestIncomplete('TODO');
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => 'Test Model',
+                    'loft' => 46,
+                    'carry_distance' => 'Test',
+                    'total_distance' => 130,
+                ]
+            )->assertUnprocessable();
     }
 
     public function test_the_total_distance_attribute_of_a_golfclub_request_is_not_required()
     {
-        $this->markTestIncomplete('TODO');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => 'Test Model',
+                    'loft' => 46,
+                    'carry_distance' => 130,
+                    'total_distance' => null,
+                ]
+            )->assertCreated();
+    }
+
+    public function test_it_validates_that_the_total_distance_attribute_of_a_golfbag_request_must_be_an_int_if_present()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => 'Test Model',
+                    'loft' => 46,
+                    'carry_distance' => 130,
+                    'total_distance' => 'Test',
+                ]
+            )->assertUnprocessable();
+    }
+
+    public function test_it_validates_that_the_loft_attribute_of_a_golfbag_request_must_be_an_int_if_present()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => 'Test Model',
+                    'loft' => 'Test',
+                    'carry_distance' => 130,
+                    'total_distance' => 130,
+                ]
+            )->assertUnprocessable();
     }
 
     public function test_the_loft_attribute_of_a_golfclub_request_is_not_required()
     {
-        $this->markTestIncomplete('TODO');
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->postJson(
+                route('golfclubs.store'),
+                [
+                    'make' => 'Test Make',
+                    'model' => 'Test Model',
+                    'loft' => null,
+                    'carry_distance' => 130,
+                    'total_distance' => 130,
+                ]
+            )->assertCreated();
     }
 }
