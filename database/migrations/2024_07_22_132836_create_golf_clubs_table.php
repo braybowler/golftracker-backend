6dead602a06fcf7eaaefd7a09fdc2e20ffc1ad74
bbowler1740 <bbowler1740@conestogac.on.ca>
2024-07-26 08:57:33 -0400

    update golf club migration to support a club_category and club_type enum property

--- database/migrations/2024_07_22_132836_create_golf_clubs_table.php
+++ database/migrations/2024_07_22_132836_create_golf_clubs_table.php
@@ -14,6 +14,26 @@ return new class extends Migration
             $table->foreignIdFor(User::class);
             $table->string('make');
             $table->string('model');
+            $table->enum(
+                'club_category',
+                [
+                    'Wedge',
+                    'Iron',
+                    'Hybrid',
+                    'Wood',
+                    'Putter'
+                ]
+            );
+            $table->enum(
+                'club_type',
+                [
+                    'LW', 'SW', 'GW', 'PW',
+                    '9i', '8i', '7i', '6i', '5i', '4i', '3i', '2i', '1i',
+                    '7h', '6h', '5h', '4h', '3h', '2h', '1h',
+                    '9w', '7w', '5w', '4w', '3w', '2w', '1w',
+                    'Putter'
+                ]
+            );
             $table->integer('carry_distance')->nullable();
             $table->integer('total_distance')->nullable();
             $table->integer('loft')->nullable();
