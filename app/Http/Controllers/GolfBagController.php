d5a4c9574b237ac352d30e87351e31d47ec3caea
Brayden Bowler <braybowler1995@gmail.com>
2024-07-31 14:51:18 -0400

    fix controller namespace issue

--- app/Http/Controllers/GolfBagController.php
+++ app/Http/Controllers/GolfBagController.php
@@ -2,25 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GolfBagRequest;
+use App\Http\Resources\GolfBagResource;
+use App\Models\GolfBag;
+use Carbon\Carbon;
+
 class GolfBagController extends Controller
 {
     public function index()
     {
+        return GolfBagResource::collection(auth()->user()->golfBags()->paginate(10));
     }
 
-    public function store($request)
+    public function store(GolfBagRequest $request)
     {
+        $validated = $request->safe()->all();
+
+        $golfBag = GolfBag::create([
+            'user_id' => auth()->id(),
+            'make' => $validated['make'],
+            'model' => $validated['model'],
+            'nickname' => $validated['nickname'] ?? '',
+        ]);
+
+        return response()->json(GolfBagResource::make($golfBag), 201);
     }
 
     public function show(string $id)
     {
+        $golfBag = GolfBag::where('user_id', auth()->id())->findOrFail($id);
+
+        return response()->json(GolfBagResource::make($golfBag));
     }
 
-    public function update($request, string $id)
+    public function update(GolfBagRequest $request, string $id)
     {
+        $validated = $request->safe()->all();
+
+        $golfBag = GolfBag::where('user_id', auth()->id())->findOrFail($id);
+
+        $golfBag->update([
+            'make' => $validated['make'],
+            'model' => $validated['model'],
+            'nickname' => $validated['nickname'] ?: '',
+            'updated_at' => Carbon::now(),
+        ]);
+
+        return response()->json(GolfBagResource::make($golfBag));
     }
 
     public function destroy(string $id)
     {
+        $golfBag = GolfBag::where('user_id', auth()->id())->findOrFail($id);
+
+        $golfBag->delete();
+
+        return response()->json([], 204);
     }
 }
